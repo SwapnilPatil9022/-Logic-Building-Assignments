4201b56945abc5018593c29c167c170d8c295321
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-10-16 23:41:00 +0530

    Add files via upload

---
+++ program10.5.c
@@ -0,0 +1,56 @@
+//Accept N numbers from user and return product of all odd elements.c
+//    Input : N : 6
+// Elements : 15 66 3 70 10 88
+//   Output : 45
+
+//    Input : N : 6
+// Elements : 44 66 72 70 10 88
+//   Output : 0
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+int Product(int Arr[],int iLength)
+{
+    int i = 0,Sum = 1;
+    for(i=0;i<iLength;i++)
+    {
+        if(Arr[i]%2 != 0)
+        {
+            Sum *= Arr[i];
+        }
+    }
+    return Sum;
+}
+
+int main()
+{
+    int iSize = 0,iRet = 0,iCnt = 0;
+    int *P = NULL;
+
+    printf("\n Enter Number Of Elements : ");
+    scanf("%d",&iSize);
+ 
+    P = (int*)malloc(iSize * sizeof(int));
+
+    if(P == NULL)
+    {
+        printf("\n Usable to Allocate Memory..");
+        return - 1;
+    }
+
+    for(iCnt = 0;iCnt<iSize;iCnt++)
+    {
+        printf("\n Enter %d Elements : ",iCnt+1);
+        scanf("%d",&P[iCnt]);
+    }
+
+    iRet = Product(P,iSize);
+
+    printf("\n Product Of Odd Element is %d\n",iRet);
+
+    free(P);
+
+    return 0;
+}
