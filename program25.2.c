51c0a6d244c30155e798ce8d48fe8c36e899d7ca
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-11-07 17:45:45 +0530

    Add files via upload

---
+++ program25.2.c
@@ -0,0 +1,35 @@
+//Write a program which accept string from user and convert it into Upper case.
+
+//Input : "Marvellous Multi OS"
+
+//Output : "MARVELLOUS MULTI OS"
+
+#include<stdio.h>
+
+void struprX(char *str)
+{
+	int iCnt = 0;
+	
+	while(*str != '\0')
+	{
+		if((*str >= 'a')&&(*str <= 'z'))
+		{
+			*str = *str - 32;
+		}
+		str++;
+	}
+}
+
+int main()
+{
+	char arr[20];
+	
+	printf("Enetr string\n");
+	scanf("%[^'\n']s",arr);
+	
+	struprX(arr);
+	
+	printf("Modified string is :  %s",arr);
+	
+	return 0;
+}
