3e0d7f58e58f307b2a2653e8dbb1ba578d1a0b37
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-11-18 00:09:15 +0530

    Add files via upload

---
+++ program32.1.c
@@ -0,0 +1,31 @@
+//Write a program which accept one number and position from user and check whether bit at that position is on or off.
+
+// Input : 10	2
+//Output : TRUE
+
+#include<stdio.h>
+typedef unsigned int UINT;
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+BOOL ChkBit(UINT uNo,UINT Bit)
+{
+    if(uNo>>(Bit-1) & 1)
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+int main()
+{
+    UINT uValue=0,uBit=0;
+
+    printf("Enter The Number => ");
+    scanf("%d",&uValue);
+    printf("Enter The Bit => ");
+    scanf("%d",&uBit);
+
+    printf("%s",(ChkBit(uValue,uBit)==TRUE)?"TRUE":"FALSE");
+
+    return 0;
+}
