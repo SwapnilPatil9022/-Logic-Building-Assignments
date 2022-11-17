151cf2ac591227c0fbbf3889d4517873d37854f4
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-11-17 23:45:43 +0530

    Add files via upload

---
+++ program31.3.c
@@ -0,0 +1,21 @@
+//Write a program which accept one number from user and toggle 7th and 10th bit of that number. Return modified number.
+
+//   Input : 137
+//	Output : 201
+
+#include<stdio.h>
+unsigned int ToggleBit(unsigned int uNo)
+{
+    return uNo^1<<6;
+}
+int main()
+{
+    unsigned int uValue=0;
+
+    printf("Enter The Number => ");
+    scanf("%d",&uValue);
+
+    printf("%d",ToggleBit(uValue));
+
+    return 0;
+}
