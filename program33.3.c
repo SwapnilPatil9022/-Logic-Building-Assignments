877a2fa46fa0c084407c3600c98901039c78515d
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-11-20 00:23:34 +0530

    Add files via upload

---
+++ program33.3.c
@@ -0,0 +1,29 @@
+//Write a program which accept one number from user and check whether 9th or 12th bit is on or off.
+
+// Input : 257
+// Output : TRUE
+
+#include<stdio.h>
+typedef unsigned int UINT;
+#define TRUE 0
+#define FALSE 1
+
+UINT ChkBit(UINT uNo)
+{
+    if(uNo & 1<<8 || uNo & 1<<11)
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+int main()
+{
+    UINT uValue=0;
+
+    printf("Enter First Number => ");
+    scanf("%d",&uValue);
+
+    printf("%s",(ChkBit(uValue)==TRUE)?"TRUE":"FALSE");
+
+    return 0;
+}
