b0e91f4eb79a0e7a9f9dc50eb36888f1a297800b
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2022-11-22 20:09:07 +0530

    Add files via upload

---
+++ program37.5.c
@@ -0,0 +1,32 @@
+//Write a recursive program which display below pattern.
+
+// Input : 6
+
+//	Output :  a		b	 c	  d 	e	 f
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+	static int i = 0;
+	static char ch = 'a';
+	
+	if(i < iNo)				//Using Reccurssion
+	{
+		printf("%c\t",ch);
+		ch++;
+		i++;
+		Display(iNo);
+	}
+}
+
+int main()
+{
+	int iValue = 0;
+	
+	printf("Enter number\n");
+	scanf("%d",&iValue);
+	
+	Display(iValue);
+	return 0;
+}
